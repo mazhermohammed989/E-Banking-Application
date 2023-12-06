af209ab95ee4442766af2f311b17b2d967aa3a4d
Mazher Mohammed <mazhermohammed989@gmail.com>
2023-12-07 01:19:29 +0530

    starting junit 4 testing

--- backend/ebanking/src/test/java/com/example/ebanking/EbankingApplicationTests.java
+++ backend/ebanking/src/test/java/com/example/ebanking/EbankingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.ebanking;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EbankingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.ebanking;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EbankingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
